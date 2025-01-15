cbdecf63e89b71ade2d7e9b72f43c2cf5f6d3340
anhdtn2403 <anhdtn2403@gmail.com>
2025-01-15 11:52:21 +0700

    server

---
+++ Exercise02/SimpleChatApplication/ChatServer/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Server
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatServer server = new ChatServer();
+            server.StartServer(5000);       //Lắng nghe trên port
+           
+        }
+    }
+}
