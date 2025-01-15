a9ae11aef29f182e493b96d503187344eaae9c94
TDHanie <luongngocthuyduongvt@gmail.com>
2025-01-15 10:56:06 +0700

    configuration

--- Exercise02/SimpleChatApplication/Client/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
+++ Exercise02/SimpleChatApplication/Client/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ae42a36f461fa28a3d8bc1929e09aff2345193")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17dfcd572807fe1784d0cbc28156db805e67705d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
