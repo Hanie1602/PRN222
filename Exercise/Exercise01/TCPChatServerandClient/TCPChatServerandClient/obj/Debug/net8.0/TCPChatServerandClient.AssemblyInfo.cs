bb7c88723dd43294a0e50f9a76d939bd36ac4b77
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-23 13:31:07 +0700

    configuration

--- Exercise/Exercise01/TCPChatServerandClient/TCPChatServerandClient/obj/Debug/net8.0/TCPChatServerandClient.AssemblyInfo.cs
+++ Exercise/Exercise01/TCPChatServerandClient/TCPChatServerandClient/obj/Debug/net8.0/TCPChatServerandClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCPChatServerandClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ae42a36f461fa28a3d8bc1929e09aff2345193")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6129fef4aaed8fd7a7aa4d38573358bffd89fbea")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCPChatServerandClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCPChatServerandClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
